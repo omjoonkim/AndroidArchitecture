481bcc2e1a1dbfb1a0e0b42ffde5dbd9b3052fd1
mesoibum joon kim <omjoonkim@gmail.com>
2017-07-22 15:12:25 +0900

    refactor : convert MoviceRepository

---
+++ app/src/main/java/iammert/com/androidarchitecture/data/MovieRepository.kt
@@ -0,0 +1,25 @@
+package iammert.com.androidarchitecture.data
+
+import android.arch.lifecycle.LiveData
+import iammert.com.androidarchitecture.data.local.dao.MovieDao
+import iammert.com.androidarchitecture.data.local.entity.MovieEntity
+import iammert.com.androidarchitecture.data.remote.MovieDBService
+import iammert.com.androidarchitecture.data.remote.model.MoviesResponse
+import retrofit2.Call
+import javax.inject.Inject
+
+
+class MovieRepository @Inject constructor(private val movieDao: MovieDao,
+                                          private val movieDBService: MovieDBService) {
+
+    fun loadPopularMovies(): LiveData<Resource<List<MovieEntity>>> =
+            object : NetworkBoundResource<List<MovieEntity>, MoviesResponse>() {
+                override fun saveCallResult(item: MoviesResponse) = movieDao.saveMovies(item.results)
+
+                override fun loadFromDb(): LiveData<List<MovieEntity>> = movieDao.loadMovies()
+
+                override fun createCall(): Call<MoviesResponse> = movieDBService.loadMovies()
+            }.asLiveData
+
+    fun getMovie(id: Int): LiveData<MovieEntity> = movieDao.getMovie(id)
+}
