47516c6ea080b82ff4bec152fd5cdb08ea0ef17a
mesoibum joon kim <omjoonkim@gmail.com>
2017-07-22 15:37:57 +0900

    refactor : convert ImageBindingAdapter

---
+++ app/src/main/java/iammert/com/androidarchitecture/databinding/ImageBindingAdapter.kt
@@ -0,0 +1,22 @@
+package iammert.com.androidarchitecture.databinding
+
+import android.databinding.BindingAdapter
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
+import iammert.com.androidarchitecture.R
+import iammert.com.androidarchitecture.data.remote.ApiConstants
+
+
+object ImageBindingAdapter {
+
+    @BindingAdapter(value = "url")
+    fun loadImageUrl(view: ImageView, url: String?) =
+            url.takeIf { it.isNullOrEmpty().not() }?.let {
+                Picasso.with(view.context)
+                        .load(ApiConstants.IMAGE_ENDPOINT_PREFIX + it)
+                        .placeholder(R.drawable.placeholder)
+                        .into(view)
+            }
+
+
+}
