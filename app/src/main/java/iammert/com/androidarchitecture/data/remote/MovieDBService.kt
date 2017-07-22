765c5285ca8370479ec8452946c23890a6e6ce12
mesoibum joon kim <omjoonkim@gmail.com>
2017-07-22 14:10:35 +0900

    refactor : convert MovieDBService

---
+++ app/src/main/java/iammert/com/androidarchitecture/data/remote/MovieDBService.kt
@@ -0,0 +1,10 @@
+package iammert.com.androidarchitecture.data.remote
+
+import iammert.com.androidarchitecture.data.remote.model.MoviesResponse
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface MovieDBService {
+    @GET("movie/popular")
+    fun loadMovies(): Call<MoviesResponse>
+}
