9baa2bc223d7fd569e4e60c5fbeecee117ceb500
mesoibum joon kim <omjoonkim@gmail.com>
2017-07-22 13:55:11 +0900

    refactor : convert ApiConstants

---
+++ app/src/main/java/iammert/com/androidarchitecture/data/remote/ApiConstants.kt
@@ -0,0 +1,8 @@
+package iammert.com.androidarchitecture.data.remote
+
+object ApiConstants {
+    const val ENDPOINT = "https://api.themoviedb.org/3/"
+    const val IMAGE_ENDPOINT_PREFIX = "https://image.tmdb.org/t/p/w500/"
+    const val API_KEY = ""
+    const val TIMEOUT_IN_SEC = 15
+}
