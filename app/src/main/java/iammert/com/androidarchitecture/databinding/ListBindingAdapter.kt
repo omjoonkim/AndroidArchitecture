b879b64b95a316ef93b5722e5882c6e64e2cecef
mesoibum joon kim <omjoonkim@gmail.com>
2017-07-29 16:53:41 +0900

    refactor : convert ListBindingAdapter

---
+++ app/src/main/java/iammert/com/androidarchitecture/databinding/ListBindingAdapter.kt
@@ -0,0 +1,22 @@
+package iammert.com.androidarchitecture.databinding
+
+import android.databinding.BindingAdapter
+import android.support.v7.widget.RecyclerView
+import iammert.com.androidarchitecture.data.Resource
+import iammert.com.androidarchitecture.ui.BaseAdapter
+
+object ListBindingAdapter {
+
+    @Suppress("UNCHECKED_CAST")
+    @JvmStatic
+    @BindingAdapter(value = "resource")
+    fun setResource(recyclerView: RecyclerView, resource: Resource<*>?) {
+
+        val adapter = recyclerView.adapter ?: return
+
+        if (resource == null || resource.data == null)
+            return
+
+        (adapter as? BaseAdapter<*, Any>)?.setData(resource.data as? List<Any> ?: emptyList())
+    }
+}
